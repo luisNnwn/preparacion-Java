3f4b4221ab5f2ee32f49aea832dc8a535c8941e3
luisNnwn <luibla.v7@gmail.com>
2025-07-16 23:17:09 -0600

    arreglado pero se puede mejorar

--- src/doWhile.java
+++ src/doWhile.java
@@ -24,15 +24,35 @@ public class doWhile {
          *
          * */
 
+        /*
+        * ejemplo guia:
+        *  C1V1=C2V2
+        * C1 = concentracion inicial //se pide int concentracionActual
+        * v1 = volumen del acido a usar //necesito pedirlo int volumenAcidoUsar;
+        * c2 = concentracion deseada //yo la doy en el enunciado
+        * v2 = volumen final total //lo calculo int volumenFinal;
+        *
+        * ejemplo 100 ml de acido al 90%
+        *
+        * 90 * 100 = 10 * v2
+        * 9000 = 10 *v2
+        * v2 = 900ml
+        *
+        * agua = 900 - 100 = 800 //lo calculo con los resultados obtenidos
+        *         v2 - vi
+        *
+        *
+        * */
+
         System.out.println("UN DO WHILE QUE DILUYE ACIDO CLORHIDRICO");
 
         boolean estaDiluido = false;
-        int concentracionDeseada = 0;
-        int volumenAcido = 0;
-        int volumenFinalTotal1 = 0;
-        int volumenFinalTotal2 = 0;
+        int concentracioActual = 0;
+        int volumenAcidoUsar = 0;
+        int volumenFinal;
         int aguita = 0;
-        String [] diluciones = {"HCL al 20%", "HCL al 15%"};
+        String formula = "";
+        String [] diluciones = {"Preparar HCL al 20%", "Preparar HCL al 15%"};
 
         Scanner scanner = new Scanner(System.in);
 
@@ -45,32 +65,42 @@ public class doWhile {
         System.out.println("4. Necesitas encontrar el volumen final total");
 
         do {
-            for (String ejercicio: diluciones) {
-                System.out.println(diluciones[0]);
-                System.out.println("Ingresa el volumen en el que vas a diluir el acido");
-                volumenAcido = scanner.nextInt();
-                scanner.nextLine();
-                System.out.println("Ingresa la concentracion deseada: ");
-                concentracionDeseada = scanner.nextInt();
-                scanner.nextLine();
-                volumenFinalTotal1 = ((20 * volumenAcido) / 20);
-                aguita = volumenFinalTotal1 - volumenAcido;
-                System.out.println("El agua a añadir es: " + aguita + " ml.");
+            System.out.println("Dilucion de HCL");
+            //imprimo el primer elemento del arreglo de strings
+            System.out.println(diluciones[0]);
+            //solicito la concentracion actual del acido (obtengo c1)
+            System.out.println("Ingrese la concentración actual de su acido");
+            concentracioActual = scanner.nextInt();
+            scanner.nextLine();
+            //ahora solicito  la los ml de acido que se van a usar (obtengo v1, y ya tengo c1, v1)
+            System.out.println("¿Cuántos ml de ácido usará? ");
+            volumenAcidoUsar = scanner.nextInt();
+            scanner.nextLine();
+            //En el enunciado doy la concentración dos, la cual es la concetracion final a la que quiero llegar
+            //ahora calculo el volumen final porque me va a servir para poder calcular el agua a agregar
+            volumenFinal = (concentracioActual * volumenAcidoUsar) / 20; //20 es la concentracion que quiero lograr
+            //una vez tengo el volumenFinal del Acido, puedo calcular el agua a agregar con la variable aguita
+            aguita = volumenFinal - volumenAcidoUsar;
+            System.out.println("El agua que debe agregar a su HCL al " + concentracioActual + "%" + " para que llegue al 20% "  +" es " + aguita + "ml.");
 
-                System.out.println(diluciones[1]);
-                System.out.println("Ingresa el volumen en el que vas a diluir el acido");
-                volumenAcido = scanner.nextInt();
-                scanner.nextLine();
-                System.out.println("Ingresa la concentracion deseada: ");
-                concentracionDeseada = scanner.nextInt();
-                scanner.nextLine();
-                volumenFinalTotal2 = ((20 * volumenAcido) / 20);
-                aguita = volumenFinalTotal1 - volumenAcido;
-                System.out.println("El agua a añadir es: " + aguita + " ml.");
 
+
+            System.out.println("¿Qué formula se uso para hacer este calculo");
+            formula = scanner.nextLine();
+            if (formula.equalsIgnoreCase("C1V1=C2V2")){
+                System.out.println("Correcto");
                 estaDiluido = true;
+            } else {
+                System.out.println("Intentalo de nuevo");
             }
-        } while (estaDiluido ==false);
+
+        }while(!estaDiluido);
     }
+
+    /*
+    * Diluir codigo
+    * agua dar al acido
+    * finalizarlo
+    * */
 
 }
